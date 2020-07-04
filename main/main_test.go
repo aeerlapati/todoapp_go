28035ce6c5fe8a110e8526b5aab132062496924b
abhinav <abhinav.eerlapati@mckesson.com>
2020-07-04 11:26:23 -0700

    canary test

---
+++ main/main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCanary(t *testing.T) {
+	t.Run("returns tweet", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/canary", nil)
+		response := httptest.NewRecorder()
+
+		canary(response, request)
+
+		got := response.Body.String()
+		want := "tweet"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
